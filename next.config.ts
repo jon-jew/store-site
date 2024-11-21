415345c7610e68381534ab0774b39fbe24451742
jon-jew <jonathan.jew@gmail.com>
2024-11-21 12:13:07 -0500

    'github-pages-next-config'

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/store-site",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
